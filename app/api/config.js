989232fd7c615595ae541ce27ef3df7e813e1356
KonstantinMogilnickov <mogilnickov.konstantin2005@gmail.com>
2024-05-20 23:58:24 +0500

    исправление

--- app/api/config.js
+++ app/api/config.js
@@ -1,4 +1,4 @@
-export const BASE_URL = 'https://pindiebackend59.nomoredomainswork.ru//api'
+export const BASE_URL = 'https://pindiebackend59.nomoredomainswork.ru/api'
 
 export const endpoints = {
   games: `${BASE_URL}/games`,
